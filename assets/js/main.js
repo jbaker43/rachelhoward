f7595ddb2b209cc52b230e8fe1d9df7bbc0cacdf
Jacob Baker <33914225+jbaker43@users.noreply.github.com>
2020-10-24 19:47:21 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,7 +58,13 @@
 			$nav_li = $nav.find('li');
 
 		// Add "middle" alignment classes if we're dealing with an even number of items.
-			
+			if ($nav_li.length % 2 == 0) {
+
+				$nav.addClass('use-left');
+				$nav_li.eq( ($nav_li.length / 2) ).addClass('is-middle');
+
+			}
+
 	// Main.
 		var	delay = 325,
 			locked = false;
